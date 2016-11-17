7fd18bf73b7569d8c423f28ec707adbf009f6ced
Thales Sabino <tluisrs@gmail.com>
2016-11-16 23:29:36 -0200

    Text format

--- Viewer/ColorStatisticsWidget.cpp
+++ Viewer/ColorStatisticsWidget.cpp
@@ -10,7 +10,7 @@ ColorStatisticsWidget::ColorStatisticsWidget(const QList<xyYColor>& Colors, QWid
 
 	auto AverageColorText = tr("Average: x: %1 y: %2").arg(ColorStats.Average.x).arg(ColorStats.Average.y);
 	auto StdDevColorText = tr("Std Dev: y: %1 y: %2").arg(ColorStats.StdDev.x).arg(ColorStats.StdDev.y);
-	auto ColorVariationText = tr("Variation: x: %1 % y: %2 %").arg(ColorStats.RelStdDev.x).arg(ColorStats.RelStdDev.y);
+	auto ColorVariationText = tr("Rel. Std Dev: x: %1 y: %2").arg(ColorStats.RelStdDev.x).arg(ColorStats.RelStdDev.y);
 
 	m_AverageColorLabel = new QLabel{ AverageColorText, this };
 	m_StdDevColorLabel = new QLabel{ StdDevColorText, this };
